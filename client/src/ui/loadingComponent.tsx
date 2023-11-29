024745a51c7acfc17532e5dbe4e7b1e321d92ddf
alexhaloETH <108292891+alexhaloETH@users.noreply.github.com>
2023-11-29 20:30:24 +0000

    test push

--- client/src/ui/loadingComponent.tsx
+++ client/src/ui/loadingComponent.tsx
@@ -20,6 +20,7 @@ export const LoadingComponent: React.FC<LoadingPageProps> = ({ setUIState }) =>{
       clearTimeout(timer);
     };
   }, []);
+  
 
   return (
     <div className="centered-div" style={{width:"100%", height:"100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
