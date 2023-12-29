f3ecb84dc66aa141dfc5b401f29efbef4b8cdbfa
alexhaloETH <108292891+alexhaloETH@users.noreply.github.com>
2023-12-29 15:19:04 +0000

    Revert "vercel push demo 1"
    This reverts commit 818aa53ad2d37c9d94f5c2b5a7f8c7a4909917c6.

--- client/src/ui/loadingComponent.tsx
+++ client/src/ui/loadingComponent.tsx
@@ -53,12 +53,12 @@ export const LoadingComponent: React.FC<LoadingPageProps> = ({ setUIState }) => 
       const create_game_prop: CreateGameProps =
       {
         account: account,
-        preparation_phase_interval: 100,
+        preparation_phase_interval: 20,
         event_interval: 5,
         erc_addr: account.address,
         reward_pool_addr: account.address,
         revenant_init_price: 10,
-        max_amount_of_revenants: 60,
+        max_amount_of_revenants: 10,
       }
   
       await create_game(create_game_prop)
@@ -106,7 +106,7 @@ export const LoadingComponent: React.FC<LoadingPageProps> = ({ setUIState }) => 
 
   useEffect(() => {
 
-    if (account.address === "0x52b5150a58bbaedba6b3baca010e209a5ad85ac37c92ce039740117eac2a2c6")
+    if (account.address === "0x517ececd29116499f4a1b64b094da79ba08dfd54a3edaa316134c41f8160973")
     {
       return;
     }
