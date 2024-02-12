c4e24bcb18024f095b7f48ebecccae3cb72bd888
Jame <jame.no.s.42@gmail.com>
2024-02-12 12:15:19 +0000

    react removed and refactor

--- client/src/ui/Pages/Trades/outpostSellingWindow.tsx
+++
@@ -1,9 +0,0 @@
-
-export const OutpostSellingWindow: React.FC = () => {
-
-    return (
-        <>
-
-        </>
-    )
-}
--- client/src/ui/Pages/Trades/outpostTradeWindow.tsx
+++
@@ -1,9 +0,0 @@
-
-export const OutpostTradeWindow: React.FC = () => {
-
-    return (
-        <>
-
-        </>
-    )
-}
